1d911956a3d2717c9ca7bccf1a814cd07cc8aec0
satoyuki <yuki01377@gmail.com>
2020-05-29 12:24:49 +0900

    init commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let win = null;
+
+/*
+テストを試す時は下のコードをコメントアウトする(自動リロード対応)
+*/
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
+function createWindow() {
+    if (win) return;
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: { nodeIntegration: true }
+    });
+    win.loadURL("file://" + __dirname + "/index.html");
+    //developer tool build
+    // win.webContents.openDevTools();
+    win.on('closed', function () { win = null; });
+}
+
+app.on('ready', createWindow);
+app.on('activate', createWindow);
